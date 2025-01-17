2b98516888c5c14793a812a0592c8108e9d048d3
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-01-17 08:54:11 +0800

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -26,7 +26,7 @@ const Sidenav = () => {
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Homez</span>
                     </a>
-                    <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <GrProjects size={20} />
                         <span className='pl-4'>Work</span>
                     </a>

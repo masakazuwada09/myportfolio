128017cac2ba39c53155b41b521f2ce9f4b7811c
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-04-17 19:26:20 +0800

    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# mxb.dev
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/57999461-2350-4da3-8788-ca4e0e6dcb30/deploy-status)](https://app.netlify.com/sites/mxb/deploys)
+
+Source code for [mxb.dev](https://mxb.dev), my personal website, built with [Eleventy](https://www.11ty.io).
+
+## License
+
+This is my personal website. I'm keeping its source code open as a learning resource, but this is not a template. You're free to re-use __parts of this codebase__ in your own site, with attribution. Just __don't copy the entire thing__, replace the content and publish it. You know - be nice about it.
---
+++ eleventy.config.js
@@ -0,0 +1,141 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import pluginRss from '@11ty/eleventy-plugin-rss'
+import pluginNavigation from '@11ty/eleventy-navigation'
+import pluginSyntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight'
+import { eleventyImageTransformPlugin as pluginImageTransform } from '@11ty/eleventy-img'
+
+import pluginPageAssets from 'eleventy-plugin-page-assets'
+import pluginShareHighlight from 'eleventy-plugin-share-highlight'
+import pluginSVGSprite from 'eleventy-plugin-svg-sprite'
+
+import filters from './utils/filters.js'
+import transforms from './utils/transforms.js'
+import shortcodes from './utils/shortcodes.js'
+import markdown from './utils/markdown.js'
+import viteHelpers from './utils/vite.js'
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const CONTENT_GLOBS = {
+    posts: 'src/posts/**/*.md',
+    drafts: 'src/drafts/**/*.md',
+    notes: 'src/notes/*.md',
+    media: '*.jpg|*.png|*.gif|*.mp4|*.webp|*.webm'
+}
+
+export default function (config) {
+    // Plugins
+    config.addPlugin(pluginRss)
+    config.addPlugin(pluginNavigation)
+    config.addPlugin(pluginSyntaxHighlight)
+    config.addPlugin(pluginPageAssets, {
+        mode: 'directory',
+        postsMatching: 'src/posts/*/*.md',
+        assetsMatching: CONTENT_GLOBS.media,
+        silent: true
+    })
+    config.addPlugin(pluginSVGSprite, {
+        path: './src/assets/icons',
+        outputFilepath: './dist/assets/icons/icons.sprite.svg'
+    })
+    config.addPlugin(pluginImageTransform, {
+        extensions: 'html',
+        formats: ['avif', 'auto'],
+        outputDir: './dist/assets/images/processed/',
+        urlPath: '/assets/images/processed/',
+        widths: ['auto'],
+        defaultAttributes: {
+            loading: 'lazy',
+            decoding: 'async'
+        }
+    })
+    config.addPlugin(pluginShareHighlight)
+
+    // Filters
+    Object.keys(filters).forEach((filterName) => {
+        config.addFilter(filterName, filters[filterName])
+    })
+
+    // Transforms
+    Object.keys(transforms).forEach((transformName) => {
+        config.addTransform(transformName, transforms[transformName])
+    })
+
+    // Shortcodes
+    config.addShortcode('icon', shortcodes.icon)
+    config.addPairedShortcode('signup', shortcodes.signup)
+    config.addPairedShortcode('callout', shortcodes.callout)
+
+    // Vite Shortcodes
+    Object.keys(viteHelpers).forEach((shortcodeName) => {
+        config.addNunjucksAsyncShortcode(
+            shortcodeName,
+            viteHelpers[shortcodeName]
+        )
+    })
+
+    // Asset Watch Targets
+    config.addWatchTarget('./src/assets')
+
+    // Markdown Parsing
+    config.setLibrary('md', markdown)
+
+    // Layouts
+    config.addLayoutAlias('base', 'base.njk')
+    config.addLayoutAlias('page', 'page.njk')
+    config.addLayoutAlias('post', 'post.njk')
+    config.addLayoutAlias('draft', 'draft.njk')
+    config.addLayoutAlias('note', 'note.njk')
+
+    // Pass-through files
+    config.addPassthroughCopy('src/site.webmanifest')
+    config.addPassthroughCopy('src/robots.txt')
+    config.addPassthroughCopy('src/assets/images')
+    config.addPassthroughCopy('src/assets/fonts')
+
+    // Deep-Merge
+    config.setDataDeepMerge(true)
+
+    // Collections: Posts
+    config.addCollection('posts', function (collection) {
+        return collection
+            .getFilteredByGlob(CONTENT_GLOBS.posts)
+            .filter((item) => item.data.permalink !== false)
+            .filter((item) => !(item.data.draft && IS_PRODUCTION))
+    })
+
+    // Collections: Drafts
+    config.addCollection('drafts', function (collection) {
+        return collection
+            .getFilteredByGlob(CONTENT_GLOBS.drafts)
+            .filter((item) => item.data.permalink !== false)
+    })
+
+    // Collections: Notes
+    config.addCollection('notes', function (collection) {
+        return collection.getFilteredByGlob(CONTENT_GLOBS.notes).reverse()
+    })
+
+    // Collections: Featured Posts
+    config.addCollection('featured', function (collection) {
+        return collection
+            .getFilteredByGlob(CONTENT_GLOBS.posts)
+            .filter((item) => item.data.featured)
+            .sort((a, b) => b.date - a.date)
+    })
+
+    // Base Config
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: 'includes',
+            layouts: 'layouts',
+            data: 'data'
+        },
+        templateFormats: ['njk', 'md', '11ty.js'],
+        htmlTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk'
+    }
+}

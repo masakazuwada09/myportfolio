7d447900c63e78a29bc13d12b761714c0e3ef83b
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-04-17 15:32:38 +0800

    updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 export default {
   plugins: {
+    '@csstools/postcss-global-data': {
+      files: ['app/global.module.css'],
+    },
+    'postcss-custom-media': {},
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};

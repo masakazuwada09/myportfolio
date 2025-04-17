5135fea5589f8ff32ce42df40dbf0cc6dfaaef36
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-04-17 15:16:53 +0800

    updates

--- scripts/dev.cjs
+++ scripts/dev.cjs
@@ -2,3 +2,4 @@ const config = require('../app/config.json');
 
 // Pop a lil' monogram in the terminal
 console.info(config.ascii);
+console.log('MINIFLARE_DISABLED:', process.env.MINIFLARE_DISABLED);

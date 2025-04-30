4dfbb51a1eab280180f7c712bc86d449d6df24f9
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-04-30 21:41:30 +0800

    updates

--- src/data/site.js
+++ src/data/site.js
@@ -1,10 +1,10 @@
 export default {
     title: 'Masakazu', 
     description:
-        'Masakazu is a professional front-end developer based in Vienna, Austria.',
+        'Masakazu is a professional front-end developer based in PH.',
     lang: 'en',
     locale: 'en_US',
-    domain: 'mxb.dev',
-    url: 'https://mxb.dev',
-    repo: 'https://github.com/maxboeck/mxb'
+    domain: 'masakazu.netlify.app',
+    url: 'https://masakazu.netlify.app',
+    repo: 'https://github.com/masakazuwada09'
 }

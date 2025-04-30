d89bbac98a7d4a93139aee6b0621316dfb17c279
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-04-30 21:36:48 +0800

    Add masakazu.netlify.app to Speedlify config

---
+++ _data/sites/masakazu.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "Masakazu",
+    url: "https://masakazu.netlify.app",
+    options: {
+      frequency: 60 * 24, // Run every 24 hours
+      freshChrome: "site", // Use a fresh Chrome instance for each site
+      skip: false // Ensure tests are not skipped
+    }
+  };

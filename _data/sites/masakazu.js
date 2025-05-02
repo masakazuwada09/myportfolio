15f23a6685e500f20c1a76c30bf25a2785f77b98
masakazuwada09 <68308456+masakazuwada09@users.noreply.github.com>
2025-05-02 18:58:44 +0800

    updates

--- _data/sites/masakazu.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "Masakazu",
-    url: "https://masakazu.netlify.app",
-    options: {
-      frequency: 60 * 24, // Run every 24 hours
-      freshChrome: "site", // Use a fresh Chrome instance for each site
-      skip: false // Ensure tests are not skipped
-    }
-  };
